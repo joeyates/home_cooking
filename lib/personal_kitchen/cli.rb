ca132aac909cba2202d654d7de26d0f55af729e4
Joe Yates <joe.g.yates@gmail.com>
2018-06-30 18:41:07 +0200

    Add 'init' command

--- lib/personal_kitchen/cli.rb
+++ lib/personal_kitchen/cli.rb
@@ -1,4 +1,27 @@
 require "thor"
 
 class PersonalKitchen::CLI < Thor
+  autoload :Init, "personal_kitchen/cli/init"
+
+  desc "init [OPTIONS]", "Creates a personal kitchen"
+  long_desc <<~EOT
+    Creates a Chef cookbook with the necessary dependencies, sets up the minimal
+    data bag, creates an SSH key, a GPG key and a pass store.
+    It then adds all files to a new git repo, and commits them.
+  EOT
+  method_option(
+    "name",
+    type: :string, required: true, banner: "the name of the kitchen",
+    aliases: ["-n"]
+  )
+  def init
+    require "personal_kitchen/cli/init"
+    Init.new(symbolized_options).run
+  end
+
+  private
+
+  def symbolized_options
+    options.reduce({}) { |h, (k, v)| h[k.intern] = v; h }
+  end
 end
---
+++ lib/personal_kitchen/cli/init.rb
@@ -0,0 +1,62 @@
+class PersonalKitchen::CLI::Init < Thor
+  require "securerandom"
+
+  include Thor::Actions
+  source_root File.expand_path("../../..", __dir__)
+
+  attr_reader :name
+
+  def initialize(name:)
+    super([])
+    @name = name
+  end
+
+  no_commands do
+    def run
+      # Here target_directory gets memoized **before** any chdir, ensuring
+      # it is relative to the initial path
+      self.destination_root = target_directory
+      directory "templates", "."
+      create_data_bag_key
+      tell_user_to_save_data_bag_key
+      install_gems
+    end
+  end
+
+  private
+
+  def create_data_bag_key
+    create_file "data_bag_key" do
+      data_bag_key
+    end
+  end
+
+  def tell_user_to_save_data_bag_key
+    puts <<~EOT
+
+      Please take note of your data bag key ('data_bag_key') -
+      without it you will be locked out of your kitchen.
+
+      Key:
+      #{data_bag_key}
+    EOT
+  end
+
+  def install_gems
+    Dir.chdir(target_directory) do
+      puts `BUNDLE_GEMFILE=#{gemfile_path} bundle install`
+    end
+  end
+
+  def target_directory
+    @target_directory ||= ::File.expand_path(name)
+  end
+
+  def gemfile_path
+    @gemfile_path ||= ::File.join(target_directory, "Gemfile")
+  end
+
+  def data_bag_key
+    @data_bag_key ||= SecureRandom.hex(100)
+  end
+end
